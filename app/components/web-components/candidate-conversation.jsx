b3b75ef6c5f68d17ff97a8c6981982173f372b39
David Fridley <ddfridley@yahoo.com>
2020-02-12 11:46:56 -0800

    stalled false is consistent

--- app/components/web-components/candidate-conversation.jsx
+++ app/components/web-components/candidate-conversation.jsx
@@ -1418,7 +1418,7 @@ class RASPUndebate extends React.Component {
         while (element = this.requestPermissionElements.shift()){
             playFunc(element);
         }
-        this.setState({ requestPermission: false, stalled: 0 });
+        this.setState({ requestPermission: false, stalled: false });
     }
 
     seat(i, seatOffset) {
