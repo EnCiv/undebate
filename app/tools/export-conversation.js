95c543c55db62f9eea179fede2089de65737ea98
David Fridley <ddfridley@yahoo.com>
2020-03-22 13:25:35 -0700

    export-conversation fixups the _id to , gets the recorder and any other non participant records too

--- app/tools/export-conversation.js
+++ app/tools/export-conversation.js
@@ -20,6 +20,11 @@ if (!global.logger) {
   })
 }
 
+function fixup_id(doc) {
+  doc._id = { $oid: doc._id.toString() } // this is how it should look when you try to import it into a
+  return doc
+}
+
 async function exportConversation(path) {
   console.info('path:', path)
   await MongoModels.connect({ uri: args.db }, { useUnifiedTopology: true })
@@ -33,20 +38,29 @@ async function exportConversation(path) {
     process.exit()
   })
   if (!parentIota || parentIota === 'null') return console.info('nothing found for path:', path)
-  iotaList.push(parentIota)
-  var newParticipantDocs = await Iota.aggregate([
-    { $match: { parentId: parentIota._id.toString(), 'component.component': 'MergeParticipants' } },
+  let parentId = parentIota._id.toString() // grab the parentId before fix up
+  iotaList.push(fixup_id(parentIota))
+  var newParticipantIotas = await Iota.aggregate([
+    { $match: { parentId, 'component.component': 'MergeParticipants' } },
     { $sort: { _id: -1 } },
     { $group: { _id: '$userId', latest: { $first: '$$ROOT' } } },
     { $replaceRoot: { newRoot: '$latest' } },
   ])
-  if (!newParticipantDocs || !newParticipantDocs.length) return console.info('no participants found for path:', path)
-  Array.prototype.push.apply(iotaList, newParticipantDocs)
+
+  if (!newParticipantIotas || !newParticipantIotas.length) return console.info('no participants found for path:', path)
+  newParticipantIotas.forEach(iota => iotaList.push(fixup_id(iota)))
+
+  var otherIotas = await Iota.find({
+    parentId,
+    'component.component': { $ne: 'MergeParticipants' },
+  })
+  otherIotas.forEach(iota => iotaList.push(fixup_id(iota)))
 
   var output = JSON.stringify(iotaList)
   fs.writeFile(path + '.json', output, function(err) {
     if (err) throw err
-    console.log(path + '.json' + ' saved')
+    console.log(path + '.json' + ' saved. ' + iotaList.length + ' records.')
+    MongoModels.disconnect()
   })
 }
 
