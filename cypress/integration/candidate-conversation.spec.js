3fc3a814e42dc88e6dee6e8b91c48a011c0cc17e
DJ Bowers <dj@dj-bowers.com>
2020-04-07 20:15:01 -0700

    add first cypress test for candidate conversation

---
+++ cypress/integration/candidate-conversation.spec.js
@@ -0,0 +1,20 @@
+describe('Candidate Conversation', () => {
+  beforeEach(() => {
+    cy.visit('/schoolboard-conversation')
+  })
+
+  it('clicking on the Begin button should cause the videos to play', () => {
+    cy.get('button')
+      .contains('Begin')
+      .click()
+
+    cy.wait(3000)
+
+    cy.get('video').each($video => {
+      const participant = $video.attr('participant')
+      const video = $video.get(0)
+      const videoIsPlaying = video.currentTime > 0 && !video.paused && !video.ended && video.readyState > 2
+      expect(videoIsPlaying, `${participant} video is playing`).to.be.true
+    })
+  })
+})
