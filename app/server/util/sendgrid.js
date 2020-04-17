8be46757af32ed53c690ded859cf900314da093e
David Fridley <ddfridley@yahoo.com>
2020-04-16 18:49:44 -0700

    sengrid using a dynamic template

--- app/server/util/sendgrid.js
+++ app/server/util/sendgrid.js
@@ -3,13 +3,16 @@
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
-  to: 'ddfridley@yahoo.com',
-  from: 'noreply@enciv.org',
-  //subject: 'Sending with Twilio SendGrid is Fun',
-  //text: 'and easy to do anywhere, even with Node.js',
-  //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  template_id: 'd-81624978fcea4731bb3ef0656992a1bf',
+  from: { email: 'noreply@enciv.org', name: 'EnCiv.org' },
+  to: 'ddfridley@gmail.com',
+  subject: 'this is not the subject in the template',
+  dynamic_template_data: {
+    //asm_global_unsubscribe_raw_url: 'https://enciv.org/unsubscribe',
+    candidate_name: 'David D. Fridley',
+    viewer_url: 'https://cc.enciv.org/schoolboard-conversation',
+  },
+  template_id: 'd-000f4e702e8b42d19c5a416febfa5e8c',
 }
-sgMail.send(msg).then(() => {
+sgMail.send(msg).then(result => {
   console.info('sent')
 }, console.error)
