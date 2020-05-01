68197313926f22f0e64e73b7fceca9b8540bce39
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-05-01 15:46:06 -0700

    candidate_questions prop added to CandidatePreamble component passing this.props.participants.moderator.agenda

--- app/components/web-components/undebate.jsx
+++ app/components/web-components/undebate.jsx
@@ -2832,6 +2832,7 @@ class RASPUndebate extends React.Component {
                 this.setState({ preambleAgreed: true })
                 noOverlay && this.beginButton()
               }}
+              candidate_questions={this.props.participants.moderator.agenda}
             />
           )}
           {ending()}
