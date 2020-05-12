41ae2047a92ebc6360f8cc5d936f052f571dd7cf
David Fridley <ddfridley@yahoo.com>
2020-05-12 15:19:04 -0700

    took out the CandidateJoin code that was never used. Converted to a functional component. Added example of candidate_question input, sliced of the last question because it's the moderators closing statment and not a candiate question

--- app/components/candidate-preamble.jsx
+++ app/components/candidate-preamble.jsx
@@ -1,11 +1,117 @@
 'use strict'
-import Join from './join'
 import React from 'react'
 import injectSheet from 'react-jss'
 import Button from './button'
 import cx from 'classnames'
-import Icon from './lib/icon'
 import ConversationHeader from './conversation-header'
+
+/**
+ * 
+ * candidate_questions might look like this:
+candidate_questions=[
+    [
+        "Introductions",
+        "1- Name",
+        "2- City and State",
+        "3- One word to describe yourself",
+        "4- What office are you running for?"
+    ],
+    [
+        "What do you love about where you live?"
+    ],
+    [
+        "What inspired you to run for office?"
+    ],
+    [
+        "If elected, what will be your top 3 priorities?"
+    ],
+    [
+        "Thank you!"
+    ]
+  ]
+ */
+
+function CandidatePreamble({ classes, onClick, agreed, bp_info, subject, candidate_questions }) {
+  const makeQuestions = questions => {
+    const showList = (className, question, index) => (
+      <ul className={className}>
+        {question.map((q, i) => (
+          <li key={index + '-' + i}>{q}</li>
+        ))}
+      </ul>
+    )
+
+    return (
+      <ul className={classes.questionList}>
+        {questions.map((question, index) =>
+          typeof question === 'string' ? (
+            <li key={index}>{question}</li>
+          ) : question.length === 1 ? (
+            <li key={index}>{question[0]}</li>
+          ) : question[0][0] >= '0' && question[0][0] <= '9' ? (
+            <li>{showList(classes.questionListInnerHeadless, question, index)}</li>
+          ) : (
+            <li>
+              {question[0]}
+              {showList(classes.questionListInner, question.slice(1), index)}
+            </li>
+          )
+        )}
+      </ul>
+    )
+  }
+
+  return (
+    <div className={cx(classes['Preamble'], agreed && classes['agreed'])}>
+      <ConversationHeader subject={subject} bp_info={bp_info} />
+      <div className={classes['Preamble-inner']}>
+        <h2>
+          Welcome{' '}
+          {bp_info && bp_info.candidate_name ? (
+            <span style={{ fontSize: '150%', fontWeight: 'bold' }}>{bp_info.candidate_name}</span>
+          ) : (
+            ''
+          )}
+        </h2>
+        <p>
+          Ballotpedia and EnCiv are teaming up to create a better way for candidates to be heard, and voters to learn
+          about their candidates.
+        </p>
+        <p>
+          You are invited to engage in an application that will include you, as part of a publicly available online
+          video conversation.
+        </p>
+        <ul className={classes.PreambleList}>
+          <li>
+            During the conversation, you will be asked questions, and your video will be recorded and stored on your
+            computer.
+          </li>
+          <li>
+            At the end of the conversation, you will be asked to review and accept EnCiv's{' '}
+            <a href="https://enciv.org/terms" target="_blank">
+              terms of service
+            </a>{' '}
+            and create an account.
+          </li>
+          <li>
+            Then, hitting the <b>Post</b> button will upload the recorded video and make it public.
+          </li>
+          <li>
+            Or, hitting the <b>Hang Up</b> button or closing this window any time before hitting the <b>Post</b> button
+            will cause any recordings to be discarded.
+          </li>
+        </ul>
+        <h2 style={{ marginBottom: '0.5rem' }}>Questions for Candidates</h2>
+        {makeQuestions(
+          candidate_questions && candidate_questions.slice(0, -1)
+        ) /* the last thing in the list is the moderators closing remarks*/}
+        <div className={classes['center']}>
+          <Button onClick={onClick}>Next</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const styles = {
   Preamble: {
@@ -44,6 +150,8 @@ const styles = {
   },
   questionList: {
     listStyleType: 'decimal',
+    marginTop: 0,
+    marginBottom: 0,
     padding: '0',
     paddingLeft: '2em',
     '& li': {
@@ -70,123 +178,4 @@ const styles = {
   },
 }
 
-class CandidateJoin extends Join {
-  onChangeActive() {} // don't do anything - there's not need to check email and password comfirmation in this case
-  render() {
-    const { classes, className, onClick } = this.props
-    const { info, successMessage, validationError } = this.state
-    return (
-      <React.Fragment>
-        {!successMessage && (
-          <React.Fragment>
-            <a className={className} href="#" onClick={this.agree.bind(this)}>
-              <Icon className={className} icon="square-o" size="2" ref="agree" name="agree" />
-            </a>
-            <span className={className}>I agree to the </span>
-            <a className={className} href="https://enciv.org/terms/" target="_blank">
-              Terms of Service
-            </a>
-            {info && <span className={className}>{info}</span>}
-            {validationError && (
-              <span className={className} style={{ color: 'red' }}>
-                {validationError}
-              </span>
-            )}
-          </React.Fragment>
-        )}
-        <div className={classes['center']}>
-          <Button onClick={this.skip.bind(this)}>Next</Button>
-        </div>
-        {successMessage && <span className={className}>{successMessage}</span>}
-      </React.Fragment>
-    )
-  }
-}
-
-class CandidatePreamble extends React.Component {
-  makeQuestions = questions => {
-    const showList = (className, question, index) => (
-      <ul className={className}>
-        {question.map((q, i) => (
-          <li key={index + '-' + i}>{q}</li>
-        ))}
-      </ul>
-    )
-
-    return (
-      <ul className={this.props.classes.questionList}>
-        {questions.map((question, index) =>
-          typeof question === 'string' ? (
-            <li key={index}>{question}</li>
-          ) : question.length === 1 ? (
-            <li key={index}>{question[0]}</li>
-          ) : question[0][0] >= '0' && question[0][0] <= '9' ? (
-            <li>{showList(this.props.classes.questionListInnerHeadless, question, index)}</li>
-          ) : (
-            <li>
-              {question[0]}
-              {showList(this.props.classes.questionListInner, question.slice(1), index)}
-            </li>
-          )
-        )}
-      </ul>
-    )
-  }
-
-  render() {
-    const { classes, onClick, agreed, bp_info } = this.props
-    return (
-      <div className={cx(classes['Preamble'], agreed && classes['agreed'])}>
-        <ConversationHeader subject={this.props.subject} bp_info={this.props.bp_info} />
-        <div className={classes['Preamble-inner']}>
-          <h2>
-            Welcome{' '}
-            {bp_info && bp_info.candidate_name ? (
-              <span style={{ fontSize: '150%', fontWeight: 'bold' }}>{bp_info.candidate_name}</span>
-            ) : (
-              ''
-            )}
-          </h2>
-          <p>
-            Ballotpedia and EnCiv are teaming up to create a better way for candidates to be heard, and voters to learn
-            about their candidates.
-          </p>
-          <p>
-            You are invited to engage in an application that will include you, as part of a publicly available online
-            video conversation.
-          </p>
-          <ul className={classes.PreambleList}>
-            <li>
-              During the conversation, you will be asked questions, and your video will be recorded and stored on your
-              computer.
-            </li>
-            <li>
-              At the end of the conversation, you will be asked to review and accept EnCiv's{' '}
-              <a href="https://enciv.org/terms" target="_blank">
-                terms of service
-              </a>{' '}
-              and create an account.
-            </li>
-            <li>
-              Then, hitting the <b>Post</b> button will upload the recorded video and make it public.
-            </li>
-            <li>
-              Or, hitting the <b>Hang Up</b> button or closing this window any time before hitting the <b>Post</b>{' '}
-              button will cause any recordings to be discarded.
-            </li>
-          </ul>
-          {/*<CandidateJoin classes={classes} userInfo={{email: (bp_info.candidate_email && bp_info.candidate_email[0]) || (bp_info.person_email && bp_info.person_email[0]), name: bp_info.candidate_name}} onChange={onClick}/>*/}
-          <h2>Questions for Candidates</h2>
-          {this.makeQuestions(this.props.candidate_questions)}
-          <div className={classes['center']}>
-            <Button onClick={onClick}>Next</Button>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
 export default injectSheet(styles)(CandidatePreamble)
-
-/*<p>When you are ready, click <b>Next</b>. After you do, you many need to authorize this app with your browser to use your camera and video.</p>*/
