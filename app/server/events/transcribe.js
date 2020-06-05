6c9a255d231e2cf3ab6abf82fa0e55533b19b80f
epg323 <epg323@ucla.edu>
2020-06-05 12:19:15 -0700

    inserts document to iota

--- app/server/events/transcribe.js
+++ app/server/events/transcribe.js
@@ -1,10 +1,12 @@
-import Transcribe from '../../models/iota'
+import Iota from '../../models/iota'
 import serverEvents from './index'
 import speech from '@google-cloud/speech'
 import https from 'https'
 import fs from 'fs'
 // add insert
 async function notifyOfNewRecording(participantIota) {
+  participantIota.subject = 'Speech to text for: ' + participantIota.subject
+  participantIota.component.transcription = []
   logger.info('~~~~~~~~~~~~~~~~', participantIota)
   let bite = participantIota.component.participant.speaking[1]
   let conv = bite.replace('.mp4', '.wav')
@@ -34,18 +36,11 @@ async function notifyOfNewRecording(participantIota) {
       config: config,
     }
     const [response] = await client.recognize(request)
-    /*const transcription = response.results
-            .map(result => result.alternatives[0].transcript)
-            .join('\n');
-        logger.info(`Transcription: ${transcription}`);
-        let info = response.results
-            .map(result => result.alternatives[0].words)*/
     let info2 = response.results[0].alternatives[0]
     logger.info('info2', info2)
 
-    //TODO:
-    //create a new iota and add the words object vvv
-    await Transcribe.create({ _id: Transcribe.ObjectID(participantIota._id) }, { $set: { words: info2 } })
+    participantIota.component.transcription.push(info2)
+    await Iota.create(participantIota)
   }
 }
 serverEvents.on(serverEvents.eNames.ParticipantCreated, notifyOfNewRecording)
