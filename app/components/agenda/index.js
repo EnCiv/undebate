a67d4e8000d77b1bc1eb03e29d51aef13bcec15a
luis-jetson <luis@jetson.ai>
2020-06-25 18:06:20 -0700

    styles for agenda container
    Co-authored-by: MrNanosh <dtzinatbakhsh@gmail.com>

--- app/components/agenda/index.js
+++ app/components/agenda/index.js
@@ -5,41 +5,30 @@ import Icon from '../lib/icon'
 export const Agenda = ({ styles, agenda, prevSection, nextSection, round }) => {
   const { stylesSet, finishUp, begin, intro, agendaStyle, classes } = styles
   return (
-    <div
-      style={agendaStyle}
-      className={cx(
-        classes['agenda'],
-        stylesSet && classes['stylesSet'],
-        finishUp && classes['finishUp'],
-        begin && classes['begin'],
-        !intro && classes['intro']
+    <div className={classes['innerAgenda']}>
+      {agenda[round] && (
+        <>
+          <div className={classes['agendaItem']}>
+            {/* <div className={classes['agendaTitle']}>
+              <button className={classes['agenda-icon-left']} onClick={prevSection}>
+                <Icon icon="chevron-left" size="1.5" name="previous-section" />
+              </button>
+              Agenda
+              <button className={classes['agenda-icon-right']} onClick={nextSection}>
+                <Icon icon="chevron-right" size="1.5" name="previous-section" />
+              </button>
+            </div> */}
+            <ul className={classes['agendaList']}>
+              {agenda[round] &&
+                agenda[round].map((item, i) => (
+                  <li className={classes['item']} key={item + i}>
+                    {item}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </>
       )}
-    >
-      <div className={classes['innerAgenda']}>
-        {agenda[round] && (
-          <>
-            <div className={classes['agendaItem']}>
-              <div className={classes['agendaTitle']}>
-                <button className={classes['agenda-icon-left']} onClick={prevSection}>
-                  <Icon icon="chevron-left" size="1.5" name="previous-section" />
-                </button>
-                Agenda
-                <button className={classes['agenda-icon-right']} onClick={nextSection}>
-                  <Icon icon="chevron-right" size="1.5" name="previous-section" />
-                </button>
-              </div>
-              <ul className={classes['agendaList']}>
-                {agenda[round] &&
-                  agenda[round].map((item, i) => (
-                    <li className={classes['item']} key={item + i}>
-                      {item}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </>
-        )}
-      </div>
     </div>
   )
 }
