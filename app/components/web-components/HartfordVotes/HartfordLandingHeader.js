61cdfd193295e0d0620c83694e414f393d17abb6
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-07-17 17:03:08 -0700

    Changed the subtitle in the hartford landing header and replaced the text title entirely with a larger logo in the h3 tags of the about page. Each logo has a size that is custom to that header

--- app/components/web-components/HartfordVotes/HartfordLandingHeader.js
+++ app/components/web-components/HartfordVotes/HartfordLandingHeader.js
@@ -178,7 +178,7 @@ const HartfordLandingHeader = () => {
       <header className={classes.header} id="landing-header">
         {isPortrait ? null : <HartfordLogo />}
         <h1 className={classes.pageTitle}>Hartford Votes ~ Vota Coalition</h1>
-        <h2 className={classes.subTitle}>Meet Hartford{"'"}s Primary Candidates for State Representatives</h2>
+        <h2 className={classes.subTitle}>Meet Hartford{"'"}s Candidates - August 11 Primary, State Representatives</h2>
         {isPortrait ? null : <EncivLogo />}
       </header>
       <div
