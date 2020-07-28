4b1792f449c79f283e9634bd9f04096e02ac7e9c
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-07-27 23:20:54 -0700

    removed unnecessary styles in HartfordLandingHeader

--- app/components/web-components/HartfordVotes/HartfordLandingHeader.js
+++ app/components/web-components/HartfordVotes/HartfordLandingHeader.js
@@ -6,14 +6,9 @@ import OrangeButton from '../../OrangeButton'
 
 const useStyles = createUseStyles({
   header: {
-    '@media (min-aspect-ratio: 1/3)': {
-      //maxHeight: '900px',
-      // make a constant height
-    },
+    '@media (min-aspect-ratio: 1/3)': {},
     width: '100vw',
     backgroundColor: 'white',
-    //height: 'calc( 100vh - 3.5em )',
-    //minHeight: '500px',
     padding: '1em 0em',
 
     //grid stuff
@@ -45,66 +40,6 @@ const useStyles = createUseStyles({
     margin: '0px',
     gridArea: 'sub-title',
     textAlign: 'center',
-  },
-  headerImages: {
-    minHeight: '310px',
-    gridArea: 'images',
-    //for overlaying questions
-
-    '& > div': {
-      maxHeight: '719px',
-      maxWidth: '1350px',
-      minHeight: '310px',
-      minWidth: '600px',
-      display: 'grid',
-      gridTemplateColumns: '1fr 3fr 3fr',
-      gridTemplateRows: '1fr 3fr 1fr',
-      gridTemplateAreas: `
-    ". . ."
-    ". . ."
-    ". questions questions"`,
-      '@media only screen and (max-device-width: 600px)': {
-        minWidth: '0',
-        minHeight: '0',
-        position: 'relative',
-        display: 'block',
-        gridTemplateColumns: 0,
-        gridTemplateRows: 0,
-        gridTemplateAreas: 0,
-      },
-      justifyContent: 'center',
-      margin: 'auto',
-      '@media (min-aspect-ratio: 19/13)': {
-        width: 'calc(( 83vh - 3.5em ) * 1.8)',
-        height: 'calc(83vh - 3.5em)',
-      },
-      '@media (min-height: 490px) and not (min-device-height: 490px)': {
-        minHeight: '368px',
-      },
-      //'@media (max-height: 540px) and (max-width: 750px)': {
-      '@media (max-width: 600px)': {
-        minHeight: '100%',
-        minWidth: '100%',
-        '@media (max-width: 660px)': {
-          minHeight: '0px',
-        },
-      },
-      width: '100%',
-      height: 'calc(100vw * 0.53)',
-    },
-    '& img': {
-      height: '100%',
-      position: 'relative',
-      zIndex: 2,
-    },
-    background:
-      'url(https://res.cloudinary.com/hf6mryjpf/image/upload/w_1200/q_auto:best/v1591726876/assets/HVC_Banner-1.jpg) top center no-repeat',
-    '@media only screen and (max-device-width: 600px)': {
-      background:
-        'url(https://res.cloudinary.com/hf6mryjpf/image/upload/w_600/q_auto:best/v1591726876/assets/HVC_Banner-1.jpg) top center no-repeat',
-    },
-    backgroundSize: 'contain',
-    position: 'relative',
   },
 })
 
