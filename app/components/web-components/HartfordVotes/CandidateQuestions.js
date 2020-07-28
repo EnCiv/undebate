b91650281b975980a992b2efd1a58d9cd3af1143
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-07-27 22:46:17 -0700

    increased the size of font for the ask questions area

--- app/components/web-components/HartfordVotes/CandidateQuestions.js
+++ app/components/web-components/HartfordVotes/CandidateQuestions.js
@@ -5,19 +5,7 @@ import { HartfordLogo, EncivLogo } from './logos'
 import OrangeButton from '../../OrangeButton'
 
 const useStyles = createUseStyles({
-  questions: {
-    alignSelf: 'center',
-    //color: 'white',
-    fontWeight: '200',
-    fontSize: '1.3em',
-    textAlign: 'right',
-    margin: '0px',
-    gridArea: 'questions',
-    width: '100%',
-    paddingRight: '1em',
-    //lineHeight: '4.3em',
-    //'@media only screen and (max-device-width: 600px)': {
-    color: '#333333',
+  container: {
     '& div a, & div': {
       height: '3em',
       lineHeight: '1.7em',
@@ -27,21 +15,19 @@ const useStyles = createUseStyles({
     marginTop: '1em',
     marginBottom: '1em',
     paddingLeft: '1em',
+    fontSize: '2em',
+  },
+  questions: {
     textAlign: 'center',
-    fontSize: '1.2em',
     fontWeight: '600',
     letterSpacing: '0.06em',
-    //},
-    //'@media only screen and (max-device-width: 415px)': {
-    //'& button': {
-    //width: '100%',
-    //},
-    //paddingLeft: '1em',
-    //textAlign: 'center',
-    //fontSize: '1.2em',
-    //fontWeight: '600',
-    //letterSpacing: '0.06em',
-    //},
+    alignSelf: 'center',
+    fontWeight: '200',
+    fontSize: '3rem',
+    margin: '0px',
+    width: '100%',
+    paddingRight: '1em',
+    color: '#333333',
   },
 })
 
@@ -49,12 +35,12 @@ const CandidateQuestions = () => {
   const classes = useStyles()
 
   return (
-    <h3 className={classes.questions}>
-      Have questions for the candidates? →{' '}
+    <div className={classes.container}>
+      <h3 className={classes.questions}>Have questions for the candidates?</h3>
       <OrangeButton href="https://forms.gle/HgDH7TpewvBeecLe9" target="_blank">
         Click here to ask Questions
       </OrangeButton>
-    </h3>
+    </div>
   )
 }
 
