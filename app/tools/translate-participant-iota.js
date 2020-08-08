e4ed477d5d1c15969c16f5ce68dc229535011f59
David <ddfridley@yahoo.com>
2020-08-07 21:34:29 -0700

    Transcribe frontend (#232)
    * made transcribe model
    
    * made schema for transcription
    
    * merge-children will merge participants and the socialpreview. meta title taken from iota.subject
    
    * transcribe prototype
    
    * GOOGLE_ANALYTICS is taken from env, if not present not used
    
    * don't fail if no iota
    
    * corrected google analytics, /canddidate-conversation uses MergeChildren
    
    * cleaned up serverReactRender no functional changes
    
    * google analytics fixed from before
    
    * removed transcribe model, run transcribe on a single event, removed transcribe event , got rid of transcribe in create partcipant
    
    * inserts document to iota
    
    * cleaned up schema, and added key to .ignore
    
    * removed importing transcibe model
    
    * testing path to .wav for production
    
    * changed path
    
    * fixed path
    
    * .
    
    * format
    
    * added transcribe into merge children
    
    * minor changes
    
    * cleaned up transcribe.js
    
    * made var plural, got rid of  enciv-transcribe.json
    
    * transcribe merge example
    
    * userId and participantId in merge-participants-into-parent
    
    * [Agenda] abstracted from CC
    
    * tab component
    
    Co-authored-by: name <MrNanosh>
    Co-authored-by: MrNanosh <dtzinatbakhsh@gmail.com>
    
    * refactored transcribe.js with useragent
    
    * fixed merging
    
    * styles 4 agenda
    
    Co-authored-by: MrNanosh <dtzinatbakhsh@gmail.com>
    
    * styles for agenda container
    
    Co-authored-by: MrNanosh <dtzinatbakhsh@gmail.com>
    
    * bare minnimum credentials
    
    * more relevant names given to gcloud keys
    
    * error message and graceful handling if transcription environment variable are not defined
    
    * added check for ENV vars in notfiy-of-new-participant
    
    * added installation instructions for speech to text api
    
    * more of the agenda is moved into the agenda component
    
    * made a transcription object and rendered it in transcript tab
    
    * changed the api call for short audio file to long audio file
    
    * fixed a merge issue
    
    * updated iota for transcript testing
    
    * added comment to transcription.js
    
    * display the words of a transcript, if there is one
    
    * useEffect connects up to play, and starts timer - words are highlighed as the person speaks
    
    * transcription trys to keep from missing the play event by checking if it is already playing
    
    * merge-latest-transcription merges multiple participants transcriptions
    
    * using google recogniseStream. has to read in the whole wav file, use wav-file-info to get the sampleRateHertz, and then stream out the in buffer file to recognizeStream. Restrured transcribe so it can be imported by transcribe-participant-iota tool
    
    * forces stereo files to mono for transcription. app/tools/translate-participant accepts viewerId. /candidate-conversation in iota has moderator tranlated. It's big
    
    * highlight spoken work rather than bold, fontSize
    
    * cleaned up styling of agenda for transcription
    
    * switch between AgendaNav and AgendaTranscript based on whether there is a transcript
    
    * sentences
    
    * iota prettier
    
    * transcribes wav and webm files, succussively approximates the fontSize to show full text in transcribe tab
    
    * default font size for transcription a little bigger
    
    Co-authored-by: epg323 <epg323@ucla.edu>
    Co-authored-by: luis-jetson <luis@jetson.ai>
    Co-authored-by: MrNanosh <dtzinatbakhsh@gmail.com>

---
+++ app/tools/translate-participant-iota.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+'use strict;'
+
+// this file makes it possible to create conversation viewer and candidate-recorder link pairs by combining information from a spreadsheet file, and an object template (below)
+// the csv file lists the election Race names, and links to the viewer and recorder for that race - if they exist
+// the program creates or updates records in the db, and generates a new spreadsheet -out.csv that includes the new links.  If a link was changes, a column shows that it was changed.
+// take pair as a parameter which is a js file that describes the data and how to operate on it
+
+const Iota = require('../models/iota')
+const MongoModels = require('mongo-models')
+import { transcribeParticipantIota, transcribeSpeakingList } from '../server/events/transcribe'
+
+// Iota uses logger
+const log4js = require('log4js')
+
+if (!global.logger) {
+  global.logger = log4js.getLogger('node')
+  log4js.configure({
+    appenders: { err: { type: 'stderr' } },
+    categories: { default: { appenders: ['err'], level: 'DEBUG' } },
+  })
+}
+
+// fetch args from command line
+var argv = process.argv
+var args = {}
+for (let arg = 2; arg < argv.length; arg++) {
+  switch (argv[arg]) {
+    case 'viewerId':
+    case 'participantId':
+    case 'db': // the mongo database URI
+      args[argv[arg]] = argv[++arg]
+      break
+    default:
+      console.error('ignoring unexpected argument:', argv[arg])
+  }
+}
+
+if (!(args.db && (args.participantId || args.viewerId))) {
+  console.error('db and ( participantId or viewerId ) are required')
+  process.exit(1)
+}
+
+function finished(result) {
+  MongoModels.disconnect()
+  process.exit(result)
+}
+
+async function main() {
+  await MongoModels.connect({ uri: args.db }, { useUnifiedTopology: true })
+  for await (const init of MongoModels.toInit) {
+    await init()
+  }
+  if (args.viewerId) {
+    try {
+      var viewerIota = await Iota.findOne({ _id: Iota.ObjectID(args.viewerId) })
+      var transcriptionList = await transcribeSpeakingList(viewerIota.webComponent.participants.moderator.speaking)
+      await Iota.updateOne(
+        { _id: viewerIota._id },
+        { $set: { 'webComponent.participants.moderator.transcriptions': transcriptionList } }
+      )
+      finished(0)
+    } catch (err) {
+      console.error('caught error trying to update viewer', err)
+      finished(1)
+    }
+  } else {
+    try {
+      const participantIota = await Iota.findOne({ _id: Iota.ObjectID(args.participantId) })
+      if (!participantIota) {
+        console.error('participantId not found', args.participantId)
+        finished(1)
+      }
+      const transcriptionIota = await transcribeParticipantIota(participantIota)
+      console.info('transcriptionIota', transcriptionIota)
+      finished(0)
+    } catch (error) {
+      logger.error('caught error trying to translate', error)
+      finished(1)
+    }
+  }
+}
+
+main()
