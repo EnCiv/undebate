92f68a20fd7863d625a32c96c11d9d26ab419673
David Fridley <ddfridley@yahoo.com>
2021-06-22 16:19:10 -0700

    got rid of undebate-line -- not used

--- app/data-components/cafe-participants.js
+++ app/data-components/cafe-participants.js
@@ -95,7 +95,7 @@ What we are trying to create:
     "subject": "School Board Candidate Conversation",
     "description": "School Board Candidate discussion has 2 candidates, and a moderator",
     "webComponent": {
-        "component": "UndebateLine",
+        "component": "Undebate",
         "participants": {
             "moderator": {
                 "speaking": [
