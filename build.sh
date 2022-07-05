4044de09ae1d946cd154b92058f6b53dd69de050
David Fridley <ddfridley@yahoo.com>
2022-07-05 12:37:57 -0700

    node rdi

--- build.sh
+++ build.sh
@@ -22,15 +22,13 @@ cp -rp node_modules/civil-server/assets/images ./assets/images
 #
 node node_modules/civil-server/dist/tools/react-directory-indexer app/web-components/ node_modules/civil-server/dist/web-components/ || {
   echo Could not build web-components
-  ls -al node_modules/.bin/react-directory-indexer
   exit 1
 }
 #
 # Update/create data-components/index.js to require all data-components in that director, and in the listed child/peer directories. Data components are used by the getIota route.
 #
-npm exec react-directory-indexer --data app/data-components/ node_modules/civil-server/dist/data-components/ || {
+node node_modules/civil-server/dist/tools/react-directory-indexer --data app/data-components/ node_modules/civil-server/dist/data-components/ || {
   echo Could not build data-components
-  ls -al node_modules/civil-server/dist/tools/react-directory-indexer
   exit 1
 }
 
