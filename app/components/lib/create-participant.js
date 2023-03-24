b6ded15c64da00ecb5b99742c94bd0013ba332f8
David Fridley <ddfridley@yahoo.com>
2023-03-24 13:16:59 -0700

    catch errors, more info

--- app/components/lib/create-participant.js
+++ app/components/lib/create-participant.js
@@ -29,122 +29,130 @@ if (typeof window !== 'undefined') {
 }
 
 export default function createParticipant(props, human, userId, name, progressFunc, listeningRound, listeningSeat) {
-  var transferred = 0
-  var totalSize = 0
-  var participant = { speaking: [], name: name }
-  var uploadQueue = []
+  try {
+    var transferred = 0
+    var totalSize = 0
+    var participant = { speaking: [], name: name }
+    var uploadQueue = []
 
-  let adjustedSpeakingBlobs = human.speakingBlobs.slice() // make a copy so we don't mutate the original
-  if (listeningSeat === 'speaking') {
-    if (typeof adjustedSpeakingBlobs[listeningRound] !== 'undefined')
-      logger.error("createParticpant didn't expect blob for listening in speakingBlobs")
-    adjustedSpeakingBlobs.splice(listeningRound, 1) // if listening is in the speaking seat - skip that round
-  }
-  function updateProgress(chunk) {
-    transferred += chunk.length
-    var percentComplete = Math.round((transferred / totalSize) * 100) + '%'
-    progressFunc && progressFunc({ progress: percentComplete, uploadComplete: false, uploadStarted: true, uploadError: false })
-  }
-
-  function upload(blob, seat, round) {
-    var file_name = userId + '-' + round + '-' + seat + new Date().toISOString().replace(/[^A-Z0-9]/gi, '') + '.mp4' // mp4 was put here to get around something with Apple - check in future
-
-    // socketIo-streams does not seem to be passing call backs (undefined is received)
-    // so we are using a socket io event to send the response back
-    const responseUrl = url => {
-      // responses don't necessarily come in order
-      if (url) {
-        logger.info('url', url)
-        url = auto_quality(url)
-        if (seat === 'speaking') {
-          // what if the come out of order -- to be determined
-          participant.speaking[round] = url // specify the round because the order is not assures - don't use push
-        } else participant.listening = url
-      } else {
-        logger.error('createParticipant.stream-upload-video failed', file_name)
-      }
-      if (
-        allThere(participant.speaking, adjustedSpeakingBlobs.length) &&
-        !!human.listeningBlob === !!participant.listening
-      ) {
-        // have all of the pieces been uploaded
-        logger.info('creat participant', participant)
-        var pIota = {
-          //participant iota
-          parentId: props.parentId || (props._id && props._id.toString()), // a viewer with a human has no parentId, but a recorder has the viewer as it's parentId
-          subject: 'Participant:' + props.subject,
-          description: 'A participant in the following discussion:' + props.description,
-          component: {
-            component: 'MergeParticipants',
-            participant: participant,
-          },
-        }
-        if (props.bp_info) {
-          // don't cause the property to exist in the Iota if there is none.
-          pIota.component.participant.bp_info = Object.assign({}, props.bp_info) // make a copy cause we are going to delete stuff
-          delete pIota.component.participant.bp_info.campaign_email
-          delete pIota.component.participant.bp_info.personal_email
-          if (props.bp_info.candidate_name) pIota.component.participant.name = props.bp_info.candidate_name
-        }
-        window.socket.emit('create-participant', pIota, result => {
-          logger.info('createParticipant participant created', result)
-        })
-      }
+    let adjustedSpeakingBlobs = human.speakingBlobs.slice() // make a copy so we don't mutate the original
+    if (listeningSeat === 'speaking') {
+      if (typeof adjustedSpeakingBlobs[listeningRound] !== 'undefined')
+        logger.error("createParticpant didn't expect blob for listening in speakingBlobs")
+      adjustedSpeakingBlobs.splice(listeningRound, 1) // if listening is in the speaking seat - skip that round
+    }
+    function updateProgress(chunk) {
+      transferred += chunk.length
+      var percentComplete = Math.round((transferred / totalSize) * 100) + '%'
+      progressFunc && progressFunc({ progress: percentComplete, uploadComplete: false, uploadStarted: true, uploadError: false })
     }
 
-    var stream = ss.createStream()
-    stream.on('error', (...args) => {
-      logger.error('createParticipant.upload socket stream error:', args)
-      progressFunc && progressFunc({ progress: `There was an error uploading: ${JSON.stringify(args, null, 2)}`, uploadComplete: false, uploadStarted: false, uploadError: true })
-    })
-      .on('end', () => {
-        var uploadArgs
-        if ((uploadArgs = uploadQueue.shift())) {
-          return upload(...uploadArgs)
+    function upload(blob, seat, round) {
+      var file_name = userId + '-' + round + '-' + seat + new Date().toISOString().replace(/[^A-Z0-9]/gi, '') + '.mp4' // mp4 was put here to get around something with Apple - check in future
+
+      // socketIo-streams does not seem to be passing call backs (undefined is received)
+      // so we are using a socket io event to send the response back
+      const responseUrl = url => {
+        // responses don't necessarily come in order
+        if (url) {
+          logger.info('url', url)
+          url = auto_quality(url)
+          if (seat === 'speaking') {
+            // what if the come out of order -- to be determined
+            participant.speaking[round] = url // specify the round because the order is not assures - don't use push
+          } else participant.listening = url
         } else {
-          progressFunc && progressFunc({ progress: 'complete.', uploadComplete: true, uploadStarted: true, uploadError: false })
-          logger.info('createParticipant upload after login complete')
+          logger.error('createParticipant.stream-upload-video failed', file_name)
         }
-      })
+        if (
+          allThere(participant.speaking, adjustedSpeakingBlobs.length) &&
+          !!human.listeningBlob === !!participant.listening
+        ) {
+          // have all of the pieces been uploaded
+          logger.info('creat participant', participant)
+          var pIota = {
+            //participant iota
+            parentId: props.parentId || (props._id && props._id.toString()), // a viewer with a human has no parentId, but a recorder has the viewer as it's parentId
+            subject: 'Participant:' + props.subject,
+            description: 'A participant in the following discussion:' + props.description,
+            component: {
+              component: 'MergeParticipants',
+              participant: participant,
+            },
+          }
+          if (props.bp_info) {
+            // don't cause the property to exist in the Iota if there is none.
+            pIota.component.participant.bp_info = Object.assign({}, props.bp_info) // make a copy cause we are going to delete stuff
+            delete pIota.component.participant.bp_info.campaign_email
+            delete pIota.component.participant.bp_info.personal_email
+            if (props.bp_info.candidate_name) pIota.component.participant.name = props.bp_info.candidate_name
+          }
+          window.socket.emit('create-participant', pIota, result => {
+            logger.info('createParticipant participant created', result)
+          })
+        }
+      }
 
-    var ssSocket = ss(window.socket)
-    //use this for debugging
-    //ssSocket._oldEmit = ssSocket.emit
-    //ssSocket.emit = ((...args) => (console.info("emit", ...args), ssSocket._oldEmit(...args)))
-    ssSocket.emit('stream-upload-video', stream, { name: file_name, size: blob.size }, responseUrl)
-
-    var bstream = ss
-      .createBlobReadStream(blob, { highWaterMark: 1024 * 200 }) // high hiwWaterMark to increase upload speed
-      .on('error', (...args) => {
-        logger.error('createParticipant.upload blob stream error:', args)
-        progressFunc && progressFunc({ progress: `There was an error uploading: ${JSON.stringify(args, null, 2)}`, uploadComplete: false, uploadStarted: false, uploadError: true })
+      var stream = ss.createStream()
+      stream.on('error', err => {
+        logger.error('createParticipant.upload socket stream error:', err.message || err)
+        progressFunc && progressFunc({ progress: `There was an error uploading: ${err.message || err}`, uploadComplete: false, uploadStarted: false, uploadError: true })
+        throw err
       })
-      .pipe(
-        through2((chunk, enc, cb) => {
-          updateProgress(chunk)
-          cb(null, chunk) // 'this' becomes this of the react component rather than this of through2 - so pass the data back in the callback
+        .on('end', () => {
+          var uploadArgs
+          if ((uploadArgs = uploadQueue.shift())) {
+            return upload(...uploadArgs)
+          } else {
+            progressFunc && progressFunc({ progress: 'complete.', uploadComplete: true, uploadStarted: true, uploadError: false })
+            logger.info('createParticipant upload after login complete')
+          }
         })
-      )
-      .pipe(stream)
 
+      var ssSocket = ss(window.socket)
+      //use this for debugging
+      ssSocket._oldEmit = ssSocket.emit
+      ssSocket.emit = ((...args) => (console.info("emit", ...args), ssSocket._oldEmit(...args)))
+      //ssSocket.emit('stream-upload-video', stream, { name: file_name, size: blob.size }, responseUrl)
+
+      var bstream = ss
+        .createBlobReadStream(blob, { highWaterMark: 1024 * 200 }) // high hiwWaterMark to increase upload speed
+        .on('error', err => {
+          logger.error('createParticipant.upload blob stream error:', err.message || err)
+          progressFunc && progressFunc({ progress: `There was an error uploading: ${err.message || err}`, uploadComplete: false, uploadStarted: false, uploadError: true })
+        })
+        .pipe(
+          through2((chunk, enc, cb) => {
+            updateProgress(chunk)
+            cb(null, chunk) // 'this' becomes this of the react component rather than this of through2 - so pass the data back in the callback
+          })
+        )
+        .pipe(stream)
+
+    }
+
+    logger.info('createParticipant.onUserUpload')
+    logger.info('createParticipant.onUserUpload', props)
+
+    for (let round = 0; round < adjustedSpeakingBlobs.length; round++) {
+      totalSize += adjustedSpeakingBlobs[round].size
+      uploadQueue.push([adjustedSpeakingBlobs[round], 'speaking', round])
+    }
+    if (human.listeningBlob) {
+      uploadQueue.push([human.listeningBlob, 'listening', 0])
+      totalSize += human.listeningBlob.size
+    }
+
+    let uploadArgs
+    logger.info("transferring:", totalSize)
+    progressFunc && progressFunc({ progress: `${totalSize} to upload`, uploadComplete: false, uploadStarted: true, uploadError: false })
+    if ((uploadArgs = uploadQueue.shift())) {
+      upload(...uploadArgs)
+    }
   }
-
-  logger.info('createParticipant.onUserUpload')
-  logger.info('createParticipant.onUserUpload', props)
-
-  for (let round = 0; round < adjustedSpeakingBlobs.length; round++) {
-    totalSize += adjustedSpeakingBlobs[round].size
-    uploadQueue.push([adjustedSpeakingBlobs[round], 'speaking', round])
-  }
-  if (human.listeningBlob) {
-    uploadQueue.push([human.listeningBlob, 'listening', 0])
-    totalSize += human.listeningBlob.size
-  }
-
-  let uploadArgs
-  logger.info("transferring:", totalSize)
-  progressFunc && progressFunc({ progress: `${totalSize} to upload`, uploadComplete: false, uploadStarted: true, uploadError: false })
-  if ((uploadArgs = uploadQueue.shift())) {
-    upload(...uploadArgs)
+  catch (error) {
+    logger.error("creatParticipant caught error", error.message || error)
+    if (window.socket.disconnected) window.socket.open()
+    // then continue
   }
 }
